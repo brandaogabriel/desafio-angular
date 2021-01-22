d6160616d20189f7ef965c0d6491f82a1f2a79fc
brandaogabriel <gabriel.brandao@ccc.ufcg.edu.br>
2021-01-21 21:57:03 -0300

    add customers

---
+++ src/app/customers/customer.ts
@@ -0,0 +1,8 @@
+export class Customer {
+  id: number;
+  name: string;
+  cpf: string;
+  gen: string;
+  birthDate: string;
+  state: string;
+}
---
+++ src/app/customers/customers-form/customers-form.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Customer } from '../customer';
+
+@Component({
+  selector: 'app-customers-form',
+  templateUrl: './customers-form.component.html',
+  styleUrls: ['./customers-form.component.sass']
+})
+export class CustomersFormComponent implements OnInit {
+
+  customer: Customer;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
